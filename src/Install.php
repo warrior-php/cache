120f0c9ad04d06e32c55fa24d0c58e8d3c26c738
Tony <57773117@qq.com>
2026-03-16 18:53:07 -0400

    Initial commit

---
+++ src/Install.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+namespace WarriorPHP\Cache;
+
+class Install
+{
+    const true WEBMAN_PLUGIN = true;
+
+    /**
+     * 源路径与目标路径的映射关系
+     *
+     * Path relations from plugin source to project destination
+     *
+     * @var array
+     */
+    protected static array $pathRelation = [
+        'config' => [
+            'path'  => 'config',
+            'files' => ['redis.php'],
+        ],
+    ];
+
+    /**
+     * Install
+     *
+     * @return void
+     */
+    public static function install(): void
+    {
+        static::installByRelation();
+    }
+
+    /**
+     * 按路径映射关系拷贝文件夹
+     * Install directories/files based on pathRelation map
+     *
+     * @return void
+     */
+    public static function installByRelation(): void
+    {
+        foreach (static::$pathRelation as $source => $info) {
+            $dest = $info['path'];
+
+            // 如果目标路径包含目录结构，确保其存在
+            if ($pos = strrpos($dest, '/')) {
+                $parent_dir = base_path() . '/' . substr($dest, 0, $pos);
+                if (!is_dir($parent_dir)) {
+                    mkdir($parent_dir, 0777, true);
+                }
+            }
+
+            // 执行目录复制
+            copy_dir(__DIR__ . "/$source", base_path() . "/$dest");
+        }
+    }
+
+    /**
+     * Uninstall
+     *
+     * @return void
+     */
+    public static function uninstall(): void
+    {
+
+    }
+}
---
+++ src/config/cache.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'default' => 'file',
+    'stores'  => [
+        'file' => [
+            'driver' => 'file',
+            'path'   => runtime_path('cache')
+        ],
+
+        'redis' => [
+            'driver'     => 'redis',
+            'connection' => 'default'
+        ],
+
+        'array' => [
+            'driver' => 'array'
+        ]
+    ]
+];
